6a0b745da85646cb14b25acb60df914efa4d6b49
jkowalski995 <kuba.ko.1995@gmail.com>
2022-06-25 22:09:01 +0200

    Password manager v0.1

---
+++ functions.py
@@ -0,0 +1,44 @@
+"""
+This file consist functions used by main program.
+
+"""
+import os
+import PySimpleGUI as Sg
+
+
+def save_file(dirr, name, login, pwd):
+    """
+    This function purpose is to save file with login and password plain string, and before it check if file with the
+    same name didn't exist.
+    :param dirr: directory where file will be saved
+    :param name: name of the file
+    :param login: login
+    :param pwd: password
+    :return: if file exist return true to raise exception in main file
+    """
+    check_if_exits = os.path.exists(dirr + name)
+    if check_if_exits:
+        return check_if_exits
+
+    os.chdir(dirr)
+    plain_string = login + ' ' + pwd
+
+    # Create and save file
+    x = open(name, 'wt')
+    x.write(plain_string)
+    x.close()
+
+
+def open_file():
+    """
+    Purpose of this function is to open existing file consisting the pair login - password and show it to the user in
+    simple popup window.
+    :return: no specified return for this function
+    """
+    # Open file
+    file_path = Sg.popup_get_file('Select password file')
+    x = open(file_path, 'r')
+    log_pwd = x.readline().split()
+
+    # Create popup with login and password
+    Sg.popup(f"Your login is: {log_pwd[0]} \nand password is: {log_pwd[1]}", title="Open file", line_width=300)
---
+++ gui.py
@@ -0,0 +1,50 @@
+"""
+
+
+"""
+import PySimpleGUI as Sg
+from functions import save_file, open_file
+
+DEFAULT_PATH = "passwords/"
+
+layout = [
+    [Sg.Text("Key name", size=(40, 1)), Sg.InputText(key="-name-")],
+    [Sg.Text("Login", size=(40, 1)), Sg.InputText(key="-login-")],
+    [Sg.Text("Password", size=(40, 1)), Sg.InputText(key="-password-")],
+    [Sg.Button("Save"), Sg.Button("Open")],
+    [Sg.Exit()]
+]
+
+if __name__ == '__main__':
+
+    window = Sg.Window(title="Password manager", layout=layout, margins=(100, 50))
+
+    while True:
+        event, values = window.read()
+        print(event, values)
+
+        # Exit the program
+        if event == Sg.WINDOW_CLOSED or event == 'Exit':
+            break
+
+        key_name = values["-name-"]
+        login = values["-login-"]
+        pwd = values["-password-"]
+
+        # Saving the password
+        if event == "Save":
+
+            # Check the values if any is empty
+            if key_name == '' or login == '' or pwd == '':
+                Sg.popup("Every field must be filled with proper value", title="Empty field!", line_width=300)
+
+            # Save the file
+            if save_file(DEFAULT_PATH, key_name, login, pwd):
+                Sg.popup(f"The file with name {key_name} already exists! \nTry another name.", title="Name!",
+                         line_width=300)
+
+        # Opening the file with password
+        if event == "Open":
+            open_file()
+
+    window.close()
